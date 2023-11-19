91b4a7135155776ad0e577bc2d97166e6ec4edcb
Diego Margoni <diegof.university@gmail.com>
2023-11-19 14:37:04 +0100

    chore: add pdm and update prj structure

---
+++ src/pyoma2/algorithm/__init__.py
@@ -0,0 +1,2 @@
+from .algorithm import EFDD_algo, FDD_algo, FSDD_algo, SSIcov_algo, SSIdat_algo
+from .run_params import FDDRunParams, SSIcovRunParams
---
+++ src/pyoma2/temp_old/pyOMA2/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1"
+
+import warnings
+
+from .OMA import Model
+from .Plot_Modes import AniMode
+from .Sel_from_plot import SelFromPlot
+from .tools import *
