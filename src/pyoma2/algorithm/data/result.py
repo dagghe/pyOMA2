6d246a8e092e27db0b85cc62fd5ccdace059cbe5
Diego Margoni <diegof.university@gmail.com>
2024-01-08 22:45:16 +0100

    fix: optional attr in result

--- src/pyoma2/algorithm/data/result.py
+++ src/pyoma2/algorithm/data/result.py
@@ -9,40 +9,34 @@ class BaseResult(BaseModel):
     """Base class for output results."""
     model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
     # dopo MPE o MPE_fromPlot
-    Fn: npt.NDArray[np.float32]  # array of natural frequencies
-    Phi: npt.NDArray[np.float32]  # array of Mode shape vectors
+    Fn: npt.NDArray[np.float32] | None = None  # array of natural frequencies
+    Phi: npt.NDArray[np.float32] | None = None  # array of Mode shape vectors
 
 
 class FDDResult(BaseResult):
     # dopo run
-    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
-
-    freq: npt.NDArray[np.float32]
-    Sy: npt.NDArray[np.float32]
-    S_val: npt.NDArray[np.float32]
-    S_vec: npt.NDArray[np.float32]
+    freq: npt.NDArray[np.float32] | None = None
+    Sy: npt.NDArray[np.float32] | None = None
+    S_val: npt.NDArray[np.float32] | None = None
+    S_vec: npt.NDArray[np.float32] | None = None
 
 class EFDDResult(BaseResult):
-    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
-
     # dopo run
-    freq: npt.NDArray[np.float32]
-    Sy: npt.NDArray[np.float32]
-    S_val: npt.NDArray[np.float32]
-    S_vec: npt.NDArray[np.float32]
+    freq: npt.NDArray[np.float32] | None = None
+    Sy: npt.NDArray[np.float32] | None = None
+    S_val: npt.NDArray[np.float32] | None = None
+    S_vec: npt.NDArray[np.float32] | None = None
     # dopo MPE, MPE_forPlot
-    Xi: npt.NDArray[np.float32]  # array of damping ratios
-    forPlot: list
+    Xi: npt.NDArray[np.float32] | None = None # array of damping ratios
+    forPlot: list | None = None
 
 class SSIResult(BaseResult):
-    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
-
     # dopo run
-    Fn_poles: npt.NDArray[np.float32]
-    xi_poles: npt.NDArray[np.float32]
-    Phi_poles: npt.NDArray[np.float32]
+    Fn_poles: npt.NDArray[np.float32] | None = None
+    xi_poles: npt.NDArray[np.float32] | None = None
+    Phi_poles: npt.NDArray[np.float32] | None = None
     # lam_poles: npt.NDArray[np.float32]
-    Lab: npt.NDArray[np.float32]
+    Lab: npt.NDArray[np.float32] | None = None
     # dopo MPE, MPE_forPlot
-    Xi: npt.NDArray[np.float32]  # array of damping ratios
-    order_out: int
+    Xi: npt.NDArray[np.float32] | None = None # array of damping ratios
+    order_out: int | None = None
