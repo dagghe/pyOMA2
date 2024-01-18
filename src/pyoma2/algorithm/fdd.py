d8844a203ac7199d3185a1b6f0b167503fbfe31f
DiegoMargoni <diegof.university@gmail.com>
2024-01-18 12:46:13 +0100

    style: linting
    Former-commit-id: 751258092108261fafe40423c3d309f9a070aa5d [formerly 1ee2cea2bf3cf03a5a15c04c2fc10d4a38327aa7]
    Former-commit-id: 72bc6515f8a6ec13a71b37b39ad9144a43cab92d

--- src/pyoma2/algorithm/fdd.py
+++ src/pyoma2/algorithm/fdd.py
@@ -2,18 +2,22 @@
 
 import typing
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-
 from pydantic import (  # controlla che i parametri passati siano quelli giusti
     validate_call,
 )
+
+from pyoma2.algorithm.base import BaseAlgorithm
+from pyoma2.algorithm.data.geometry import Geometry1, Geometry2
 from pyoma2.algorithm.data.result import (
-    FDDResult, EFDDResult,
+    EFDDResult,
+    FDDResult,
 )
 from pyoma2.algorithm.data.run_params import (
-    FDDRunParams, EFDDRunParams,
+    EFDDRunParams,
+    FDDRunParams,
 )
 from pyoma2.functions import (  # noqa: F401
     FDD_funct,
@@ -22,13 +26,17 @@ from pyoma2.functions import (  # noqa: F401
     plot_funct,
     pLSCF_funct,
 )
-from pyoma2.functions.plot_funct import( 
-plt_nodes,plt_lines,plt_quiver,plt_surf,set_ax_options,set_view)
-
-from pyoma2.plot.Sel_from_plot import SelFromPlot
-from pyoma2.algorithm.base import BaseAlgorithm
-from pyoma2.algorithm.data.geometry import Geometry1, Geometry2
+from pyoma2.functions.plot_funct import (
+    plt_lines,
+    plt_nodes,
+    plt_quiver,
+    plt_surf,
+    set_ax_options,
+    set_view,
+)
 from pyoma2.plot.anim_mode import AniMode
+from pyoma2.plot.Sel_from_plot import SelFromPlot
+
 
 # =============================================================================
 # FREQUENCY DOMAIN DECOMPOSITION
@@ -93,32 +101,31 @@ class FDD_algo(BaseAlgorithm[FDDRunParams, FDDResult]):
         self.result.Fn = Fn_FDD
         self.result.Phi = Phi_FDD
 
-    def plot_CMIF(self, 
-                  freqlim: typing.Optional[float] = None,
-                  nSv: typing.Optional[int] = "all",
-                  ) -> typing.Any:
+    def plot_CMIF(
+        self,
+        freqlim: typing.Optional[float] = None,
+        nSv: typing.Optional[int] = "all",
+    ) -> typing.Any:
         """Tobe implemented, plot for FDD, EFDD, FSDD
         Mode Identification Function (MIF)
         """
         if not self.result:
             raise ValueError("Run algorithm first")
         fig, ax = plot_funct.CMIF_plot(
-            S_val=self.result.S_val,
-            freq=self.result.freq,
-            freqlim=freqlim,
-            nSv=nSv
+            S_val=self.result.S_val, freq=self.result.freq, freqlim=freqlim, nSv=nSv
         )
         return fig, ax
 
-    def plot_mode_g1(self,
-                     Geo1: Geometry1,
-                     mode_numb : int,
-                     scaleF: int = 1,
-                     view: typing.Literal["3D","xy","xz","yz","x","y","z"] = "3D",
-                     remove_fill: True | False =True, 
-                     remove_grid: True | False =True, 
-                     remove_axis: True | False =True
-                     ) -> typing.Any:
+    def plot_mode_g1(
+        self,
+        Geo1: Geometry1,
+        mode_numb: int,
+        scaleF: int = 1,
+        view: typing.Literal["3D", "xy", "xz", "yz", "x", "y", "z"] = "3D",
+        remove_fill: True | False = True,
+        remove_grid: True | False = True,
+        remove_axis: True | False = True,
+    ) -> typing.Any:
         """Tobe implemented, plot for FDD, EFDD, FSDD
         Mode Identification Function (MIF)
         """
@@ -127,64 +134,70 @@ class FDD_algo(BaseAlgorithm[FDDRunParams, FDDResult]):
             raise ValueError("Run algorithm first")
 
         # Select the (real) mode shape
-        phi = self.result.Phi[:,int(mode_numb-1)].real
-        fn = self.result.Fn[int(mode_numb-1)]
+        phi = self.result.Phi[:, int(mode_numb - 1)].real
+        fn = self.result.Fn[int(mode_numb - 1)]
 
-        fig = plt.figure(figsize=(10,10),tight_layout=True)
-        ax = fig.add_subplot(111, projection='3d')
+        fig = plt.figure(figsize=(10, 10), tight_layout=True)
+        ax = fig.add_subplot(111, projection="3d")
 
         # set title
-        ax.set_title(f"Mode nr. {mode_numb}, $f_n$={fn:.3f}Hz"
-                          )
+        ax.set_title(f"Mode nr. {mode_numb}, $f_n$={fn:.3f}Hz")
 
         # plot sensors' nodes
-        sens_coord= Geo1.sens_coord[["x","y","z"]].to_numpy()
-        plt_nodes(ax,sens_coord,color="red")
+        sens_coord = Geo1.sens_coord[["x", "y", "z"]].to_numpy()
+        plt_nodes(ax, sens_coord, color="red")
 
         # plot Mode shape
-        plt_quiver(ax,sens_coord,Geo1.sens_dir*phi.reshape(-1,1),
-                   scaleF=scaleF,names=Geo1.sens_names)
+        plt_quiver(
+            ax,
+            sens_coord,
+            Geo1.sens_dir * phi.reshape(-1, 1),
+            scaleF=scaleF,
+            names=Geo1.sens_names,
+        )
 
         # Check that BG nodes are defined
         if Geo1.bg_nodes is not None:
             # if True plot
-            plt_nodes(ax,Geo1.bg_nodes,color="gray",alpha=0.5)
+            plt_nodes(ax, Geo1.bg_nodes, color="gray", alpha=0.5)
             # Check that BG lines are defined
             if Geo1.bg_lines is not None:
                 # if True plot
-                plt_lines(ax,Geo1.bg_nodes,Geo1.bg_lines,
-                      color="gray",alpha=0.5)
+                plt_lines(ax, Geo1.bg_nodes, Geo1.bg_lines, color="gray", alpha=0.5)
             if Geo1.bg_surf is not None:
                 # if True plot
-                plt_surf(ax,Geo1.bg_nodes,Geo1.bg_surf
-                         ,alpha=0.1)
+                plt_surf(ax, Geo1.bg_nodes, Geo1.bg_surf, alpha=0.1)
 
         # check for sens_lines
         if Geo1.sens_lines is not None:
             # if True plot
-            plt_lines(ax,sens_coord,Geo1.sens_lines,
-                      color="red")
+            plt_lines(ax, sens_coord, Geo1.sens_lines, color="red")
 
         # Set ax options
-        set_ax_options(ax,bg_color="w",
-                       remove_fill=remove_fill,
-                       remove_grid=remove_grid,
-                       remove_axis=remove_axis)
+        set_ax_options(
+            ax,
+            bg_color="w",
+            remove_fill=remove_fill,
+            remove_grid=remove_grid,
+            remove_axis=remove_axis,
+        )
 
         # Set view
         set_view(ax, view=view)
         return fig, ax
 
-
-    def plot_mode_g2(self,
-                     Geo2: Geometry2,
-                     mode_numb: typing.Optional[int],
-                     scaleF: int = 1,
-                     view:typing.Literal["3D","xy","xz","yz","x","y","z"]="3D",
-                     remove_fill: True | False = True, 
-                     remove_grid: True | False = True, 
-                     remove_axis: True | False = True,
-                     *args, **kwargs) -> typing.Any:
+    def plot_mode_g2(
+        self,
+        Geo2: Geometry2,
+        mode_numb: typing.Optional[int],
+        scaleF: int = 1,
+        view: typing.Literal["3D", "xy", "xz", "yz", "x", "y", "z"] = "3D",
+        remove_fill: True | False = True,
+        remove_grid: True | False = True,
+        remove_axis: True | False = True,
+        *args,
+        **kwargs,
+    ) -> typing.Any:
         """Tobe implemented, plot for FDD, EFDD, FSDD
         Mode Identification Function (MIF)
         """
@@ -192,81 +205,91 @@ class FDD_algo(BaseAlgorithm[FDDRunParams, FDDResult]):
             raise ValueError("Run algorithm first")
 
         # Select the (real) mode shape
-        fn = self.result.Fn[int(mode_numb-1)]
-        phi = self.result.Phi[:,int(mode_numb-1)].real*scaleF
+        fn = self.result.Fn[int(mode_numb - 1)]
+        phi = self.result.Phi[:, int(mode_numb - 1)].real * scaleF
         # create mode shape dataframe
-        df_phi = pd.DataFrame({
-            "sName":Geo2.sens_names,
-            "Phi":phi
-            },)
-        mapping = dict(zip(df_phi['sName'], df_phi['Phi']))
+        df_phi = pd.DataFrame(
+            {"sName": Geo2.sens_names, "Phi": phi},
+        )
+        mapping = dict(zip(df_phi["sName"], df_phi["Phi"]))
         # reshape the mode shape dataframe to fit the pts coord
         df_phi_map = Geo2.sens_map.replace(mapping).astype(float)
         # add together coordinates and mode shape displacement
-        newpoints = Geo2.pts_coord.add(df_phi_map*Geo2.sens_sign, fill_value=0)
+        newpoints = Geo2.pts_coord.add(df_phi_map * Geo2.sens_sign, fill_value=0)
         # extract only the displacement array
-        newpoints = newpoints.to_numpy()[:,1:]
+        newpoints = newpoints.to_numpy()[:, 1:]
 
         # create fig and ax
-        fig = plt.figure(figsize=(8,8),tight_layout=True)
-        ax = fig.add_subplot(111, projection='3d')
-        
+        fig = plt.figure(figsize=(8, 8), tight_layout=True)
+        ax = fig.add_subplot(111, projection="3d")
+
         # set title
-        ax.set_title(f"Mode nr. {mode_numb}, $f_n$={fn:.3f}Hz"
-                          )
+        ax.set_title(f"Mode nr. {mode_numb}, $f_n$={fn:.3f}Hz")
 
         # Check that BG nodes are defined
         if Geo2.bg_nodes is not None:
             # if True plot
-            plot_funct.plt_nodes(ax,Geo2.bg_nodes,color="gray",alpha=0.5)
+            plot_funct.plt_nodes(ax, Geo2.bg_nodes, color="gray", alpha=0.5)
             # Check that BG lines are defined
             if Geo2.bg_lines is not None:
                 # if True plot
-                plot_funct.plt_lines(ax,Geo2.bg_nodes,Geo2.bg_lines,
-                      color="gray",alpha=0.5)
+                plot_funct.plt_lines(
+                    ax, Geo2.bg_nodes, Geo2.bg_lines, color="gray", alpha=0.5
+                )
             if Geo2.bg_surf is not None:
                 # if True plot
-                plot_funct.plt_surf(ax,Geo2.bg_nodes,Geo2.bg_surf
-                         ,alpha=0.1)
+                plot_funct.plt_surf(ax, Geo2.bg_nodes, Geo2.bg_surf, alpha=0.1)
         # PLOT MODE SHAPE
-        plot_funct.plt_nodes(ax,newpoints,color="red")
+        plot_funct.plt_nodes(ax, newpoints, color="red")
         # check for sens_lines
         if Geo2.sens_lines is not None:
             # if True plot
-            plot_funct.plt_lines(ax,newpoints,Geo2.sens_lines,
-                      color="red")
+            plot_funct.plt_lines(ax, newpoints, Geo2.sens_lines, color="red")
 
         # Set ax options
-        plot_funct.set_ax_options(ax,bg_color="w",
-                       remove_fill=remove_fill,
-                       remove_grid=remove_grid,
-                       remove_axis=remove_axis)
+        plot_funct.set_ax_options(
+            ax,
+            bg_color="w",
+            remove_fill=remove_fill,
+            remove_grid=remove_grid,
+            remove_axis=remove_axis,
+        )
 
         # Set view
         plot_funct.set_view(ax, view=view)
 
         return fig, ax
 
-    def anim_mode_g2(self,
-                     Geo2: Geometry2,
-                     mode_numb: typing.Optional[int],
-                     scaleF: int = 1,
-                     view: typing.Literal["3D","xy","xz","yz","x","y","z"] = "3D",
-                     remove_fill: True | False =True, 
-                     remove_grid: True | False =True, 
-                     remove_axis: True | False =True,
-                     *args, **kwargs) -> typing.Any:
+    def anim_mode_g2(
+        self,
+        Geo2: Geometry2,
+        mode_numb: typing.Optional[int],
+        scaleF: int = 1,
+        view: typing.Literal["3D", "xy", "xz", "yz", "x", "y", "z"] = "3D",
+        remove_fill: True | False = True,
+        remove_grid: True | False = True,
+        remove_axis: True | False = True,
+        *args,
+        **kwargs,
+    ) -> typing.Any:
         """Tobe implemented, plot for FDD, EFDD, FSDD
         Mode Identification Function (MIF)
         """
         if self.result.Fn is None:
             raise ValueError("Run algorithm first")
-            
+
         Res = self.result
         print("Running Anim Mode FDD")
-        AniMode(Geo=Geo2,Res=Res, mode_numb=mode_numb,
-                scaleF=scaleF,view=view,remove_axis=remove_axis,
-                remove_fill=remove_fill,remove_grid=remove_grid)
+        AniMode(
+            Geo=Geo2,
+            Res=Res,
+            mode_numb=mode_numb,
+            scaleF=scaleF,
+            view=view,
+            remove_axis=remove_axis,
+            remove_fill=remove_fill,
+            remove_grid=remove_grid,
+        )
         print("...end AniMode FDD...")
 
 
@@ -349,9 +372,9 @@ class EFDD_algo(FDD_algo[EFDDRunParams, EFDDResult]):
         self.result.Phi = Phi_FDD
         self.result.forPlot = forPlot
 
-    def plot_FIT(self,
-                 freqlim: typing.Optional[float] = None,
-                 *args, **kwargs) -> typing.Any:
+    def plot_FIT(
+        self, freqlim: typing.Optional[float] = None, *args, **kwargs
+    ) -> typing.Any:
         """Tobe implemented, plot for FDD, EFDD, FSDD
         Mode Identification Function (MIF)
         """
@@ -361,7 +384,7 @@ class EFDD_algo(FDD_algo[EFDDRunParams, EFDDResult]):
         fig, ax = plot_funct.EFDD_FIT_plot(
             Fn=self.result.Fn,
             Xi=self.result.Xi,
-            PerPlot = self.result.forPlot,
+            PerPlot=self.result.forPlot,
             freqlim=freqlim,
         )
         return fig, ax
--- src/pyoma2/plot/anim_mode.py
+++ src/pyoma2/plot/anim_mode.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Jan  8 11:33:32 2024
 
 @author: dpa
 """
 
+import tkinter as tk
 import typing
 
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d.art3d import Line3D
 
-import tkinter as tk
-
-from pyoma2.functions import Gen_funct,plot_funct
-from pyoma2.algorithm.data.result import BaseResult
 from pyoma2.algorithm.data.geometry import Geometry2
+from pyoma2.algorithm.data.result import BaseResult
+from pyoma2.functions import Gen_funct, plot_funct
 
 
 def animate_scatters(iteration, Data, scatters, lines=None, sens_lines=None):
@@ -30,37 +27,48 @@ def animate_scatters(iteration, Data, scatters, lines=None, sens_lines=None):
     # Update scatter points
     if Data.shape[1] == 3:
         for i in range(Data.shape[0]):
-            scatters[i]._offsets3d = (Data[i,0:1,iteration], Data[i,1:2,iteration], Data[i,2:,iteration])
+            scatters[i]._offsets3d = (
+                Data[i, 0:1, iteration],
+                Data[i, 1:2, iteration],
+                Data[i, 2:, iteration],
+            )
     else:
         for i in range(Data.shape[0]):
-            scatters[i].set_offsets((Data[i,0,iteration], Data[i,1,iteration]))
+            scatters[i].set_offsets((Data[i, 0, iteration], Data[i, 1, iteration]))
 
     # Update lines
     if lines is not None and sens_lines is not None:
         for line, (idx1, idx2) in zip(lines, sens_lines):
-            line.set_data(Data[[idx1, idx2], 0, iteration], Data[[idx1, idx2], 1, iteration])
+            line.set_data(
+                Data[[idx1, idx2], 0, iteration], Data[[idx1, idx2], 1, iteration]
+            )
             line.set_3d_properties(Data[[idx1, idx2], 2, iteration])
             line.set_color("r")
         return scatters + lines
     else:
         return scatters
+
+
 # =============================================================================
 # PLOTTING CLASS
 # =============================================================================
 
+
 class AniMode:
     def __init__(
-            self, 
-            Geo:Geometry2, 
-            Res:BaseResult, 
-            mode_numb: int,
-            scaleF: int = 1,
-            view:typing.Literal["3D","xy","xz","yz","x","y","z"]="3D",
-            remove_fill: True | False = True, 
-            remove_grid: True | False = True, 
-            remove_axis: True | False = True,
-            *args, **kwargs) -> typing.Any:
-        """ 
+        self,
+        Geo: Geometry2,
+        Res: BaseResult,
+        mode_numb: int,
+        scaleF: int = 1,
+        view: typing.Literal["3D", "xy", "xz", "yz", "x", "y", "z"] = "3D",
+        remove_fill: True | False = True,
+        remove_grid: True | False = True,
+        remove_axis: True | False = True,
+        *args,
+        **kwargs,
+    ) -> typing.Any:
+        """
         Bla bla bla
         """
         self.Geo = Geo
@@ -77,9 +85,9 @@ class AniMode:
         self.sens_names = self.Geo.sens_names
 
         self.Fn = self.Res.Fn
-        self.fn = self.Res.Fn[int(self.mode_numb-1)]
+        self.fn = self.Res.Fn[int(self.mode_numb - 1)]
 
-        self.phi = self.Res.Phi[:, int(self.mode_numb-1)]
+        self.phi = self.Res.Phi[:, int(self.mode_numb - 1)]
 
         if self.Res.Xi is not None:
             self.Xi = self.Res.Xi
@@ -88,72 +96,75 @@ class AniMode:
 
         self.root = tk.Tk()
         # self.root.title(f'Mode n*{self.mode_numb}, MCF = {self.MCF}')
-        self.fig = Figure(figsize=(8, 8),tight_layout=True)
-        
+        self.fig = Figure(figsize=(8, 8), tight_layout=True)
+
         canvas = FigureCanvasTkAgg(self.fig, self.root)
-        canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-        
-# =============================================================================
+        canvas.get_tk_widget().pack(side="top", fill="both", expand=1)
+
+        # =============================================================================
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         NavigationToolbar2Tk(canvas, self.root)
 
         canvas.draw()
         self.plot3D()
-# =============================================================================
+        # =============================================================================
 
         # Integrate matplotlib figure
         self.root.mainloop()
-    
+
     def plot3D(self):
-        nr_iter=200
+        nr_iter = 200
 
         # create mode shape dataframe
-        df_phi = pd.DataFrame({
-            "sName":self.sens_names,
-            "Phi":self.phi.real*self.scaleF
-            },)
-        mapping = dict(zip(df_phi['sName'], df_phi['Phi']))
+        df_phi = pd.DataFrame(
+            {"sName": self.sens_names, "Phi": self.phi.real * self.scaleF},
+        )
+        mapping = dict(zip(df_phi["sName"], df_phi["Phi"]))
         # reshape the mode shape dataframe to fit the pts coord
         sens_map = self.Geo.sens_map
         df_phi_map = sens_map.replace(mapping).astype(float)
         # add together coordinates and mode shape displacement
-        newpoints = self.nodes_coord.add(df_phi_map*self.Geo.sens_sign, fill_value=0)
-        newpoints1 = self.nodes_coord.add(df_phi_map*self.Geo.sens_sign*(-1), fill_value=0)
+        newpoints = self.nodes_coord.add(df_phi_map * self.Geo.sens_sign, fill_value=0)
+        newpoints1 = self.nodes_coord.add(
+            df_phi_map * self.Geo.sens_sign * (-1), fill_value=0
+        )
         # extract only the displacement array
-        newpoints = newpoints.to_numpy()[:,1:]
-        newpoints1 = newpoints1.to_numpy()[:,1:]
-        oldpoints= self.nodes_coord.to_numpy()[:,1:]
+        newpoints = newpoints.to_numpy()[:, 1:]
+        newpoints1 = newpoints1.to_numpy()[:, 1:]
+        oldpoints = self.nodes_coord.to_numpy()[:, 1:]
         # Create trajectories (first quarter than reverse and concatenate)
-        traj1 = np.linspace(oldpoints, newpoints, int(nr_iter/4))
-        traj2 = np.concatenate([traj1,traj1[::-1]])
+        traj1 = np.linspace(oldpoints, newpoints, int(nr_iter / 4))
+        traj2 = np.concatenate([traj1, traj1[::-1]])
         # Create trajectories (third quarter than reverse and concatenate)
-        traj3 = np.linspace(oldpoints, newpoints1, int(nr_iter/4))
-        traj4 = np.concatenate([traj3,traj3[::-1]])
+        traj3 = np.linspace(oldpoints, newpoints1, int(nr_iter / 4))
+        traj4 = np.concatenate([traj3, traj3[::-1]])
         # concatenate all traj
-        traj5= np.concatenate([traj2,traj4])
+        traj5 = np.concatenate([traj2, traj4])
         # Finally assemble data
         Data = np.moveaxis(traj5, 0, -1)
 
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        
-        self.ax.set_title(f"Mode nr. {self.mode_numb}, $f_n$={self.fn:.3f}Hz"
-                          )
+        self.ax = self.fig.add_subplot(111, projection="3d")
+
+        self.ax.set_title(f"Mode nr. {self.mode_numb}, $f_n$={self.fn:.3f}Hz")
         # Setting the axes properties
-        self.ax.set_xlim3d([np.min(Data[:,0,:]), np.max(Data[:,0,:])])
-        self.ax.set_xlabel('X')
+        self.ax.set_xlim3d([np.min(Data[:, 0, :]), np.max(Data[:, 0, :])])
+        self.ax.set_xlabel("X")
 
-        self.ax.set_ylim3d([np.min(Data[:,1,:]), np.max(Data[:,1,:])])
-        self.ax.set_ylabel('Y')
+        self.ax.set_ylim3d([np.min(Data[:, 1, :]), np.max(Data[:, 1, :])])
+        self.ax.set_ylabel("Y")
 
-        self.ax.set_zlim3d([np.min(Data[:,2,:]), np.max(Data[:,2,:])])
-        self.ax.set_zlabel('Z')
-        
+        self.ax.set_zlim3d([np.min(Data[:, 2, :]), np.max(Data[:, 2, :])])
+        self.ax.set_zlabel("Z")
+
         # Set ax options
-        plot_funct.set_ax_options(self.ax,bg_color="w",
-                       remove_fill=self.remove_fill,
-                       remove_grid=self.remove_grid,
-                       remove_axis=self.remove_axis)
+        plot_funct.set_ax_options(
+            self.ax,
+            bg_color="w",
+            remove_fill=self.remove_fill,
+            remove_grid=self.remove_grid,
+            remove_axis=self.remove_axis,
+        )
 
         # Set view
         plot_funct.set_view(self.ax, view=self.view)
@@ -161,43 +172,57 @@ class AniMode:
         # Check that BG nodes are defined
         if self.Geo.bg_nodes is not None:
             # if True plot
-            plot_funct.plt_nodes(self.ax,self.Geo.bg_nodes,color="gray",alpha=0.5)
+            plot_funct.plt_nodes(self.ax, self.Geo.bg_nodes, color="gray", alpha=0.5)
             # Check that BG lines are defined
             if self.Geo.bg_lines is not None:
                 # if True plot
-                plot_funct.plt_lines(self.ax,self.Geo.bg_nodes,self.Geo.bg_lines,
-                      color="gray",alpha=0.5)
+                plot_funct.plt_lines(
+                    self.ax, self.Geo.bg_nodes, self.Geo.bg_lines, color="gray", alpha=0.5
+                )
             if self.Geo.bg_surf is not None:
                 # if True plot
-                plot_funct.plt_surf(self.ax,self.Geo.bg_nodes,self.Geo.bg_surf
-                         ,alpha=0.1)
+                plot_funct.plt_surf(
+                    self.ax, self.Geo.bg_nodes, self.Geo.bg_surf, alpha=0.1
+                )
 
         # Initialize scatters
-        scatters = [self.ax.scatter(Data[i,0:1,0], Data[i,1:2,0], Data[i,2:,0],
-                                    color="r") 
-                    for i in range(Data.shape[0]) 
-                    ]
+        scatters = [
+            self.ax.scatter(Data[i, 0:1, 0], Data[i, 1:2, 0], Data[i, 2:, 0], color="r")
+            for i in range(Data.shape[0])
+        ]
         if self.Geo.sens_lines is not None:
             # Initialize lines based on sens_lines
-            lines = [self.ax.add_line(Line3D([], [], []))
-                     for _ in range(len(self.Geo.sens_lines))]
+            lines = [
+                self.ax.add_line(Line3D([], [], []))
+                for _ in range(len(self.Geo.sens_lines))
+            ]
             # Creating the Animation object
             self.ani = animation.FuncAnimation(
-                self.fig, animate_scatters, nr_iter, 
-                fargs=(Data, scatters, lines, self.Geo.sens_lines),interval=20, 
-                blit=False, repeat=True)
+                self.fig,
+                animate_scatters,
+                nr_iter,
+                fargs=(Data, scatters, lines, self.Geo.sens_lines),
+                interval=20,
+                blit=False,
+                repeat=True,
+            )
 
         else:
             self.ani = animation.FuncAnimation(
-                self.fig, animate_scatters, nr_iter, fargs=(Data, scatters),
-                interval=20, blit=False, repeat=True
-                )
+                self.fig,
+                animate_scatters,
+                nr_iter,
+                fargs=(Data, scatters),
+                interval=20,
+                blit=False,
+                repeat=True,
+            )
 
         plt.show()
 
     def on_closing(self):
-        print('called animode on_closing()')
+        print("called animode on_closing()")
         self.root.quit()
-        print('called animode root.quit()')
+        print("called animode root.quit()")
         self.root.destroy()
-        print('called animode root.destroy()')
+        print("called animode root.destroy()")
