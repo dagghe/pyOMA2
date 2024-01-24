12da7c4500f9aa0ff317182564f36115642c7d3f
DiegoMargoni <diegof.university@gmail.com>
2024-01-24 14:19:36 +0100

    style: linting

--- src/pyoma2/Example_MSpoSER.py
+++ src/pyoma2/Example_MSpoSER.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jan 24 09:00:24 2024
 
@@ -8,31 +7,60 @@ Created on Wed Jan 24 09:00:24 2024
 import os
 
 import numpy as np
-from scipy import signal
 import pandas as pd
+from scipy import signal
 
 from pyoma2.algorithm import (
-    EFDD_algo,
-    FDD_algo,
-    FSDD_algo,
     SSIcov_algo,
-    SSIdat_algo,
 )
-from pyoma2.OMA import SingleSetup, MultiSetup_PoSER
+from pyoma2.OMA import MultiSetup_PoSER, SingleSetup
 
 # import data files
-set1=np.load(r"src\pyoma2\test_data\3SL\set1.npy",allow_pickle=True)
-set2=np.load(r"src\pyoma2\test_data\3SL\set2.npy",allow_pickle=True)
-set3=np.load(r"src\pyoma2\test_data\3SL\set3.npy",allow_pickle=True)
+set1 = np.load(r"src\pyoma2\test_data\3SL\set1.npy", allow_pickle=True)
+set2 = np.load(r"src\pyoma2\test_data\3SL\set2.npy", allow_pickle=True)
+set3 = np.load(r"src\pyoma2\test_data\3SL\set3.npy", allow_pickle=True)
 
 # import geometry files
 _fold = r"src/pyoma2/test_data/3SL"
 # Names of the channels
-Names=[
-    ["ch1_1","ch2_1","ch3_1","ch4_1","ch5_1","ch6_1","ch7_1","ch8_1","ch9_1","ch10_1"],
-    ["ch1_2","ch2_2","ch3_2","ch4_2","ch5_2","ch6_2","ch7_2","ch8_2","ch9_2","ch10_2"],
-    ["ch1_3","ch2_3","ch3_3","ch4_3","ch5_3","ch6_3","ch7_3","ch8_3","ch9_3","ch10_3"],
-    ]
+Names = [
+    [
+        "ch1_1",
+        "ch2_1",
+        "ch3_1",
+        "ch4_1",
+        "ch5_1",
+        "ch6_1",
+        "ch7_1",
+        "ch8_1",
+        "ch9_1",
+        "ch10_1",
+    ],
+    [
+        "ch1_2",
+        "ch2_2",
+        "ch3_2",
+        "ch4_2",
+        "ch5_2",
+        "ch6_2",
+        "ch7_2",
+        "ch8_2",
+        "ch9_2",
+        "ch10_2",
+    ],
+    [
+        "ch1_3",
+        "ch2_3",
+        "ch3_3",
+        "ch4_3",
+        "ch5_3",
+        "ch6_3",
+        "ch7_3",
+        "ch8_3",
+        "ch9_3",
+        "ch10_3",
+    ],
+]
 # Common Backgroung nodes and lines
 BG_nodes = np.loadtxt(_fold + os.sep + "BG_nodes.txt")
 BG_lines = np.loadtxt(_fold + os.sep + "BG_lines.txt").astype(int)
@@ -82,20 +110,29 @@ ssicov1.plot_STDiag(freqlim=20)
 ssicov2.plot_STDiag(freqlim=20)
 ssicov3.plot_STDiag(freqlim=20)
 
-#%% =============================================================================
+# %% =============================================================================
 # ss1.MPE_fromPlot("SSIcov1", freqlim=25)
 # ss2.MPE_fromPlot("SSIcov2", freqlim=25)
 # ss3.MPE_fromPlot("SSIcov3", freqlim=25)
 # ------------------------------------------------------------------------------
-ss1.MPE("SSIcov1", 
-        sel_freq=[2.63,2.69,3.43,8.29,8.42,10.62,14.00,14.09,17.57], order=50)
-ss2.MPE("SSIcov2", 
-        sel_freq=[2.63,2.69,3.43,8.29,8.42,10.62,14.00,14.09,17.57], order=40)
-ss3.MPE("SSIcov3", 
-        sel_freq=[2.63,2.69,3.43,8.29,8.42,10.62,14.00,14.09,17.57], order=40)
+ss1.MPE(
+    "SSIcov1",
+    sel_freq=[2.63, 2.69, 3.43, 8.29, 8.42, 10.62, 14.00, 14.09, 17.57],
+    order=50,
+)
+ss2.MPE(
+    "SSIcov2",
+    sel_freq=[2.63, 2.69, 3.43, 8.29, 8.42, 10.62, 14.00, 14.09, 17.57],
+    order=40,
+)
+ss3.MPE(
+    "SSIcov3",
+    sel_freq=[2.63, 2.69, 3.43, 8.29, 8.42, 10.62, 14.00, 14.09, 17.57],
+    order=40,
+)
 
-#%% =============================================================================
-################ INITIALIZATION OF THE MULTISETUP #############################
+# %% =============================================================================
+# ############### INITIALIZATION OF THE MULTISETUP #############################
 # reference indices
 ref_ind = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
 # Creating Multi setup
@@ -108,16 +145,16 @@ res_ssicov = dict(result[SSIcov_algo.__name__])
 
 # Define geometry1
 msp.def_geo1(
-    Names, # Names of the channels
-    sens_coord, # coordinates of the sensors
-    sens_dir, # sensors' direction
-    bg_nodes=BG_nodes, # BG nodes
-    bg_lines=BG_lines # BG lines
+    Names,  # Names of the channels
+    sens_coord,  # coordinates of the sensors
+    sens_dir,  # sensors' direction
+    bg_nodes=BG_nodes,  # BG nodes
+    bg_lines=BG_lines,  # BG lines
 )
 
 # Define geometry 2
 msp.def_geo2(
-    Names, # Names of the channels
+    Names,  # Names of the channels
     pts_coord,
     sens_map,
     order_red="xy",
@@ -137,4 +174,3 @@ algoRes = result[SSIcov_algo.__name__]
 msp.plot_mode_g1(Algo_Res=algoRes, Geo1=msp.Geo1, mode_numb=2, view="3D", scaleF=2)
 # Animate mode 3 (geometry 2)
 msp.anim_mode_g2(Algo_Res=algoRes, Geo2=msp.Geo2, mode_numb=3, view="xy", scaleF=3)
-
