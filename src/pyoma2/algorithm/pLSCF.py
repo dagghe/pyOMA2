d69cba60f5973c4358cea3295ac987dd9ae52d7f
Dag <dagpasca@hotmail.com>
2024-02-24 11:53:13 +0100

    docu+fix

--- src/pyoma2/algorithm/pLSCF.py
+++ src/pyoma2/algorithm/pLSCF.py
@@ -284,7 +284,7 @@ class pLSCF_algo(BaseAlgorithm[pLSCFRunParams, pLSCFResult, typing.Iterable[floa
         if not self.result:
             raise ValueError("Run algorithm first")
 
-        fig, ax = plot_funct.Cluster_SSI_plot(
+        fig, ax = plot_funct.Cluster_plot(
             Fn=self.result.Fn_poles,
             Sm=self.result.xi_poles,
             Lab=self.result.Lab,
